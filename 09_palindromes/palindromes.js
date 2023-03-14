154b85dc437161ee24b2741d533bfe432fb2f988
Gichuki Mwangi <robertmwangi732@gmail.com>
2023-03-14 14:28:06 -0400

    Added extra solutions to the palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //need to remove all non-alphanumeric characters
+    //we'll use regular expressions to do that
+    //create a variable to store str without the non-alpha num characters
+
+    let reg = /[\W_]/g //removes unwanted characters from the string
+
+    //convert the string to lowerCase
+
+    let newStr = str.toLowerCase().replace(reg, '')
+
+    //split the string into an array of strings
+    //use the reverse method to reverse the array
+    //join the elements in the array
+
+    let reverseStr = newStr.split('').reverse().join('');
+
+    return reverseStr === newStr;
 
 };
 
